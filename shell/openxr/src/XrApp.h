481c2f6996de09eb22dcda21f398bcb7e526b79c
BattleAxeVR <belakampis@gmail.com>
2024-03-11 21:33:55 -0400

    Disable single-pass stereo

--- shell/openxr/src/XrApp.h
+++ shell/openxr/src/XrApp.h
@@ -137,7 +137,7 @@ class XrApp {
   std::array<glm::mat4, kNumViews> viewTransforms_;
   std::array<glm::vec3, kNumViews> cameraPositions_;
 
-  bool useSinglePassStereo_ = true;
+  bool useSinglePassStereo_ = false;
   bool useQuadLayerComposition_ = false;
 
   // If useSinglePassStereo_ is true, only one XrSwapchainProvider will be created.
