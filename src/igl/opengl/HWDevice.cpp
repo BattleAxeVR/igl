902b17430e12704d91f2b24550cff7064b69d0fd
Bela Kampis <belakampis@gmail.com>
2024-04-12 19:29:07 -0400

    Update HWDevice.cpp

--- src/igl/opengl/HWDevice.cpp
+++ src/igl/opengl/HWDevice.cpp
@@ -27,8 +27,8 @@ std::unique_ptr<IDevice> HWDevice::create(const HWDeviceDesc& /*desc*/,
                                           RenderingAPI api,
                                           EGLNativeWindowType nativeWindow,
                                           Result* outResult) {
-  //auto context = createContext(api, nativeWindow, outResult);
-  auto context = createOffscreenContext(api, 1920, 1920, outResult);
+  auto context = createContext(api, nativeWindow, outResult);
+  //auto context = createOffscreenContext(api, 1920, 1920, outResult);
 
   if (!context) {
     Result::setResult(outResult, Result::Code::RuntimeError, "context is null");
